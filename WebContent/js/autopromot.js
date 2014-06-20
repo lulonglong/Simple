3647c553c5c376f182b72c5827b0d5f1b78dfbee
lulonglong <luguo3000@126.com>
2014-06-20 18:39:52 +0800

    change promot

--- WebContent/js/autopromot.js
+++ WebContent/js/autopromot.js
@@ -1,5 +1,5 @@
-//高亮索引
 var highlightindex = -1;
+var timeDelay;
 
 // 设置文本框的内容
 function setContent(list, index) {
@@ -7,30 +7,91 @@ function setContent(list, index) {
 	$("#siteSug").val(context);
 }
 
-// 设置背景颜色
-function setBackgroundColor(con, index, color) {
-	con.eq(index).css("background-color", color);
-}
-
+// add ul list
 function addSugList() {
-	// add ul list
 	var name_container = $(".site-name");
 	var url_container = $(".site-url");
 	var name_container_offset = name_container.offset();
 	var url_container_offset = url_container.offset();
 	$(document.body).append(
 			'<ul class="mysite-suggest" id="mysite-name-suggest" ></ul>');
-	$(".mysite-suggest").css("position", "absolute").css("border",
-			"1px solid #707a86").css("background-color", "#fff").css("top",
+	$(".mysite-suggest").css("top",
 			(name_container_offset.top + name_container.height())).css("left",
 			name_container_offset.left).width(
 			url_container_offset.left - name_container_offset.left
 					+ url_container.width());
 }
 
+function generateSiteList(data) {
+	var name = data.split(',')[0];
+	var url = data.split(',')[1];
+	return '<li><div class="sug-name">' + name + '</div>	<div class="sug-url">'
+			+ url + '</div></li>';
+}
+
+function applyCSS() {
+	$(document.body)
+			.append(
+					'<style type="text/css">.mysite-suggest{position:absolute;border:1px solid #707a86;background-color:#fff}.mysite-suggest li{overflow:hidden;height:30px;cursor:pointer}.mysite-suggest .sug-url,.mysite-suggest .sug-name{display:inline;overflow:hidden;float:left;padding-left:6px;font-size:14px;height:30px;line-height:30px;text-overflow:ellipsis;white-space:nowrap}.mysite-suggest .sug-url{margin-right:12px;width:206px}.mysite-suggest .sug-name{margin-right:80px;width:188px}.mysite-suggest .hover{background-color:#f2f8ff}</style>');
+	$(document).on("mouseover",".mysite-suggest li", function() {
+		changeHoverClass(this);
+	});
+}
+/**
+ * // 鼠标离开 newNode .mouseout(function() { $( this) .css( "background-color",
+ * "white"); }); // 鼠标点击 newNode .click(function() { setContent( con,
+ * highlightindex); highlightindex = -1; sugContainer .hide(); });
+ * 
+ * @param li
+ */
+function changeHoverClass(li) {
+	$(".mysite-suggest .hover").removeClass();
+	$(li).addClass("hover");
+}
+
+function getSite(word,sug_api_url){
+	// 将文本框的内容发到服务器
+	$
+			.ajax({
+				type : "get",
+				url : sug_api_url
+						+ word,
+				dataType : "jsonp",// 数据类型为jsonp
+				jsonp : "cb",// 服务端用于接收callback调用的function名的参数
+				jsonpCallback : "callback",
+				success : function(
+						data) {
+					// 清除上一次的列表
+					var sugContainer = $(".mysite-suggest");
+					sugContainer
+							.html(" ");
+					$(
+							data)
+							.each(
+									
+									function() {
+										sugContainer
+												.append(generateSiteList(this));
+									});
+					// 当返回的数据长度大于0才显示
+					if ($(
+							sugContainer)
+							.children(
+									"li").length > 0) {
+						sugContainer
+								.show();
+					} else {
+						sugContainer
+								.hide();
+					}
+				}
+			});
+}
+
 $(document)
 		.ready(
 				function() {
+					applyCSS();
 					addSugList();
 					var sugContainer = $(".mysite-suggest");
 					$("#mysite-name-add,#mysite-url-add")
@@ -43,9 +104,6 @@ $(document)
 												|| keyCode == 8
 												|| keyCode == 46
 												|| keyCode == 32) {
-
-											// 清除上一次的列表
-											sugContainer.html(" ");
 
 											// 获得文本框内容
 											var word;
@@ -60,162 +118,43 @@ $(document)
 												sug_api_url = "http://suggest.h.qhimg.com/index.php?biz=websiteurl&fmt=jsonp&word=";
 											}
 
-											var timeDelay;
 											if (word.trim() == "") {
 												sugContainer.hide();
 												highlightindex = -1;
 												return;
 											}
 
-											// 取消上次提交
+											// 若上次间隔时间未到，先取消上次提交
+											
 											window.clearTimeout(timeDelay);
-											// 延迟提交，这边设置的为400ms
-											timeDelay = window.setTimeout(
-											// 将文本框的内容发到服务器
-											 $.ajax({  
-											        type : "get",  
-											        async:false,  
-											        url : sug_api_url + word,  
-											        dataType : "jsonp",//数据类型为jsonp  
-											        jsonp: "cb",//服务端用于接收callback调用的function名的参数  
-											        jsonpCallback:"callback",
-											        success : function(data){  
-											        	alert(data); 
-														// var jqObj = $(data);
-														// // 找到所有的word节点
-														// var wordNodes = jqObj
-														// .find("word");
-														// wordNodes
-														// .each(function(
-														// i) {
-														// // 获得返回的单词内容
-														// var wordNode =
-														// $(this);
-														// var newNode = $(
-														// "<div>")
-														// .html(
-														// wordNode
-														// .text())
-														// .attr(
-														// "id",
-														// i)
-														// .addClass(
-														// "pro");
-														// // 将返回内容附加到页面
-														// newNode
-														// .appendTo(sugContainer);
-														// // 处理鼠标事件
-														// var con = $(
-														// "#sugContainer")
-														// .children(
-														// "div");
-														// // 鼠标经过
-														// newNode
-														// .mouseover(function()
-														// {
-														// if (highlightindex !=
-														// -1) {
-														// setBackgroundColor(
-														// con,
-														// highlightindex,
-														// "white");
-														// }
-														// highlightindex = $(
-														// this)
-														// .attr(
-														// "id");
-														// $(
-														// this)
-														// .css(
-														// "background-color",
-														// "#ADD8E6");
-														// setContent(
-														// con,
-														// highlightindex);
-														// });
-														// // 鼠标离开
-														// newNode
-														// .mouseout(function()
-														// {
-														// $(
-														// this)
-														// .css(
-														// "background-color",
-														// "white");
-														// });
-														// // 鼠标点击
-														// newNode
-														// .click(function() {
-														// setContent(
-														// con,
-														// highlightindex);
-														// highlightindex = -1;
-														// sugContainer
-														// .hide();
-														// });
-														// }); // each
-														//
-														// // 当返回的数据长度大于0才显示
-														// if (wordNodes.length
-														// > 0) {
-														// sugContainer
-														// .show();
-														// } else {
-														// sugContainer
-														// .hide();
-														// }
-													}}), 400); // settimeout
+											timeDelay = window
+													.setTimeout(
+														'getSite("'+word+'","'+sug_api_url+'")', 400); // settimeout
 										} else {
 											// 获得返回框中的值
-											var rvalue = $("#sugContainer")
-													.children("div");
-											// 上下键
-											if (keyCode == 38 || keyCode == 40) {
-												// 向上
-												if (keyCode == 38) {
-													if (highlightindex != -1) {
-														setBackgroundColor(
-																rvalue,
-																highlightindex,
-																"white");
-														highlightindex--;
-													}
-													if (highlightindex == -1) {
-														setBackgroundColor(
-																rvalue,
-																highlightindex,
-																"white");
-														highlightindex = rvalue.length - 1;
-													}
-													setBackgroundColor(rvalue,
-															highlightindex,
-															"#ADD8E6");
-													setContent(rvalue,
-															highlightindex);
-												}
-												// 向下
-												if (keyCode == 40) {
-													if (highlightindex != rvalue.length) {
-														setBackgroundColor(
-																rvalue,
-																highlightindex,
-																"white");
-														highlightindex++;
-													}
-													if (highlightindex == rvalue.length) {
-														setBackgroundColor(
-																rvalue,
-																highlightindex,
-																"white");
-														highlightindex = 0;
-													}
-													setBackgroundColor(rvalue,
-															highlightindex,
-															"#ADD8E6");
-													setContent(rvalue,
-															highlightindex);
+											var sitelis = $(sugContainer)
+													.children("li");
+
+											// 向上
+											if (keyCode == 38) {
+												if (highlightindex > 0) {
+													highlightindex--;
+												} else {
+													highlightindex = sitelis.length - 1;
 												}
 											}
+											// 向下
+											if (keyCode == 40) {
+												if (highlightindex < sitelis.length) {
+													highlightindex++;
+												} else {
+													highlightindex = 0;
+												}
+												// setContent(rvalue,
+												// highlightindex);
+											}
+
+											changeHoverClass($(sitelis)[highlightindex]);
 											// 回车键
 											if (keyCode == 13) {
 												if (highlightindex != -1) {
