bcfaa6e5a94c06e975b653b29ee9e5cfc86fe2c3
lulonglong <luguo3000@126.com>
2014-06-20 23:58:26 +0800

    add promot

--- WebContent/js/autopromot.js
+++ WebContent/js/autopromot.js
@@ -2,9 +2,9 @@ var highlightindex = -1;
 var timeDelay;
 
 // 设置文本框的内容
-function setContent(list, index) {
-	var context = list.eq(index).text();
-	$("#siteSug").val(context);
+function setContent(li) {
+	$("#mysite-name-add").val($(li).children(":first-child").text());
+	$("#mysite-url-add").val($(li).children(":last-child").text());
 }
 
 // add ul list
@@ -32,60 +32,53 @@ function generateSiteList(data) {
 function applyCSS() {
 	$(document.body)
 			.append(
-					'<style type="text/css">.mysite-suggest{position:absolute;border:1px solid #707a86;background-color:#fff}.mysite-suggest li{overflow:hidden;height:30px;cursor:pointer}.mysite-suggest .sug-url,.mysite-suggest .sug-name{display:inline;overflow:hidden;float:left;padding-left:6px;font-size:14px;height:30px;line-height:30px;text-overflow:ellipsis;white-space:nowrap}.mysite-suggest .sug-url{margin-right:12px;width:206px}.mysite-suggest .sug-name{margin-right:80px;width:188px}.mysite-suggest .hover{background-color:#f2f8ff}</style>');
-	$(document).on("mouseover",".mysite-suggest li", function() {
+					'<style type="text/css">.mysite-suggest{display:none;position:absolute;border:1px solid #707a86;background-color:#fff}.mysite-suggest li{overflow:hidden;height:30px;cursor:pointer}.mysite-suggest .sug-url,.mysite-suggest .sug-name{display:inline;overflow:hidden;float:left;padding-left:6px;font-size:14px;height:30px;line-height:30px;text-overflow:ellipsis;white-space:nowrap}.mysite-suggest .sug-url{margin-right:12px;width:206px}.mysite-suggest .sug-name{margin-right:80px;width:188px}.mysite-suggest .hover{background-color:#f2f8ff}</style>');
+	$(document).on("mouseover", ".mysite-suggest li", function() {
 		changeHoverClass(this);
 	});
+
+	$(document).on("mousedown", ".mysite-suggest li", function() {
+		setContent(this);
+	});
+
+	$(document).on("keydown", "#mysite-name-add,#mysite-url-add", function(e) {
+		if (e.keyCode == 38)
+			return false;
+	});
 }
-/**
- * // 鼠标离开 newNode .mouseout(function() { $( this) .css( "background-color",
- * "white"); }); // 鼠标点击 newNode .click(function() { setContent( con,
- * highlightindex); highlightindex = -1; sugContainer .hide(); });
- * 
- * @param li
- */
+
 function changeHoverClass(li) {
 	$(".mysite-suggest .hover").removeClass();
 	$(li).addClass("hover");
 }
 
-function getSite(word,sug_api_url){
+function getSite(word, sug_api_url) {
 	// 将文本框的内容发到服务器
-	$
-			.ajax({
-				type : "get",
-				url : sug_api_url
-						+ word,
-				dataType : "jsonp",// 数据类型为jsonp
-				jsonp : "cb",// 服务端用于接收callback调用的function名的参数
-				jsonpCallback : "callback",
-				success : function(
-						data) {
-					// 清除上一次的列表
-					var sugContainer = $(".mysite-suggest");
-					sugContainer
-							.html(" ");
-					$(
-							data)
-							.each(
-									
-									function() {
-										sugContainer
-												.append(generateSiteList(this));
-									});
-					// 当返回的数据长度大于0才显示
-					if ($(
-							sugContainer)
-							.children(
-									"li").length > 0) {
-						sugContainer
-								.show();
-					} else {
-						sugContainer
-								.hide();
-					}
-				}
+	$.ajax({
+		type : "get",
+		url : sug_api_url + word,
+		dataType : "jsonp",// 数据类型为jsonp
+		jsonp : "cb",// 服务端用于接收callback调用的function名的参数
+		jsonpCallback : "callback",
+		success : function(data) {
+			// 清除上一次的列表
+			var sugContainer = $(".mysite-suggest");
+			sugContainer.html(" ");
+			$(data).each(
+
+			function() {
+				sugContainer.append(generateSiteList(this));
 			});
+			// 当返回的数据长度大于0才显示
+			if ($(sugContainer).children("li").length > 0) {
+				sugContainer.show();
+				highlightindex = -1;
+			} else {
+				sugContainer.html(" ");
+				sugContainer.hide();
+			}
+		}
+	});
 }
 
 $(document)
@@ -94,9 +87,19 @@ $(document)
 					applyCSS();
 					addSugList();
 					var sugContainer = $(".mysite-suggest");
+
+					// 当文本框失去焦点时隐藏提示框
+					$("#mysite-name-add,#mysite-url-add").focusout(function() {
+						sugContainer.html(" ");
+						sugContainer.hide();
+					});
+
 					$("#mysite-name-add,#mysite-url-add")
 							.keyup(
 									function(event) {
+										// 若上次间隔时间未到，先取消上次提交
+										window.clearTimeout(timeDelay);
+
 										var myevent = event || window.event;
 										var keyCode = myevent.keyCode;
 										// 字母，退格，删除，空格
@@ -119,58 +122,39 @@ $(document)
 											}
 
 											if (word.trim() == "") {
+												sugContainer.html(" ");
 												sugContainer.hide();
-												highlightindex = -1;
 												return;
 											}
 
-											// 若上次间隔时间未到，先取消上次提交
-											
-											window.clearTimeout(timeDelay);
-											timeDelay = window
-													.setTimeout(
-														'getSite("'+word+'","'+sug_api_url+'")', 400); // settimeout
-										} else {
-											// 获得返回框中的值
-											var sitelis = $(sugContainer)
-													.children("li");
-
-											// 向上
-											if (keyCode == 38) {
-												if (highlightindex > 0) {
-													highlightindex--;
-												} else {
-													highlightindex = sitelis.length - 1;
-												}
-											}
-											// 向下
-											if (keyCode == 40) {
-												if (highlightindex < sitelis.length) {
-													highlightindex++;
-												} else {
-													highlightindex = 0;
-												}
-												// setContent(rvalue,
-												// highlightindex);
-											}
-
-											changeHoverClass($(sitelis)[highlightindex]);
-											// 回车键
-											if (keyCode == 13) {
-												if (highlightindex != -1) {
-													setContent(rvalue,
-															highlightindex);
-													highlightindex = -1;
-													sugContainer.hide();
-												} else {
-													alert($("#siteSug").val());
-												}
-											}
+											timeDelay = window.setTimeout(
+													'getSite("' + word + '","'
+															+ sug_api_url
+															+ '")', 400); // settimeout
 										}
-									});// 键盘抬起
 
-					// 当文本框失去焦点时隐藏提示框
-					$("#mysite-name-add,#mysite-url-add").focusout(function() {
-						sugContainer.hide();
-					});
+										// 获得返回框中的值
+										var sitelis = $(sugContainer).children(
+												"li");
+										// 向上
+										if (keyCode == 38&&sitelis.length>0) {
+											if (highlightindex > 0) {
+												highlightindex--;
+											} else {
+												highlightindex = sitelis.length - 1;
+											}
+											setContent($(sitelis)[highlightindex]);
+											changeHoverClass($(sitelis)[highlightindex]);
+										}
+										// 向下
+										if (keyCode == 40&&sitelis.length>0) {
+											if (highlightindex < sitelis.length - 1) {
+												highlightindex++;
+											} else {
+												highlightindex = 0;
+											}
+											setContent($(sitelis)[highlightindex]);
+											changeHoverClass($(sitelis)[highlightindex]);
+										}
+									});
 				});
